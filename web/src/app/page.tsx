550a8d36cfd5d04f657b8ff0e026628775568ff3
thiagorochatr <thiagord2016@gmail.com>
2023-05-17 13:02:01 -0300

    cores personalizadas e favicon

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua capsula do tempo</h1>
-      <h1 className="font-alt text-4xl font-bold">Sua capsula do tempo</h1>
-    </div>
-  )
+  return <div className="h-screen bg-zinc-950 p-6 text-zinc-50"></div>
 }
